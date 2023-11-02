a876343a303f70e62a7616ed0749a8aac59deda1
GoktugYmsk <gogtug033@outlook.com>
2023-11-02 09:29:09 +0300

    The necessary project environment was created

---
+++ src/components/content/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './index.scss'
+
+function Content() {
+    return (
+        <div className='container-content' >
+            Content
+        </div>
+    )
+}
+
+export default Content
