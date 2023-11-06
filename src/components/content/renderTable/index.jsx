fea45f90749e9631e424b53652e68ed6b2088410
alpayyy <115874416+alpayyy@users.noreply.github.com>
2023-11-06 22:33:10 +0300

    Update

--- src/components/content/renderTable/index.jsx
+++ src/components/content/renderTable/index.jsx
@@ -1,29 +1,35 @@
-import React from 'react'
-
+import React from 'react';
+import './RenderTable.css'; 
 function RenderTable({ backendData, period }) {
-    const chartData = backendData[period];
-    return (
-        <table>
-            <thead>
-                <tr>
-                    <th>Date</th>
-                    <th>Category</th>
-                    <th>Amount</th>
-                    <th>Type</th>
-                </tr>
-            </thead>
-            <tbody>
-                {chartData.map((data, index) => (
-                    <tr key={index}>
-                        <td>{data.date}</td>
-                        <td>{data.category}</td>
-                        <td>{data.amount}</td>
-                        <td>{data.type}</td>
-                    </tr>
-                ))}
-            </tbody>
-        </table>
-    );
-};
+  const chartData = backendData[period];
+
+ 
+  const tableHeaders = ["Date", "Category", "Amount", "Type"];
+
+  return (
+    <div className="table-container">
+      <h2>{period.charAt(0).toUpperCase() + period.slice(1)} Transactions</h2>
+      <table>
+        <thead>
+          <tr>
+            {tableHeaders.map((header, index) => (
+              <th key={index}>{header}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {chartData.map((data, index) => (
+            <tr key={index}>
+              <td>{data.date}</td>
+              <td>{data.category}</td>
+              <td>{data.amount}</td>
+              <td>{data.type}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
 
 export default RenderTable;
--- src/components/content/renderTransactionForm/index.jsx
+++ src/components/content/renderTransactionForm/index.jsx
@@ -1,111 +1,117 @@
-import React from 'react'
+import React from 'react';
 
-function RenderTransactionForm({ setNewTransaction, newTransaction,
-    backendData, period, isTransactionFormValid
-}) {
+const RenderTransactionForm = ({
+  setNewTransaction,
+  newTransaction,
+  backendData,
+  period,
+  isTransactionFormValid
+}) => {
+  const handleAddTransaction = () => {
+    backendData[period].push(newTransaction);
 
-    const handleAddTransaction = () => {
-        backendData[period].push(newTransaction);
+    if (period !== "daily") {
+      backendData["daily"].push(newTransaction);
+    }
+    if (period !== "weekly") {
+      backendData["weekly"].push(newTransaction);
+    }
+    if (period !== "monthly") {
+      backendData["monthly"].push(newTransaction);
+    }
 
-        if (period !== "daily") {
-            backendData["daily"].push(newTransaction);
-        }
-        if (period !== "weekly") {
-            backendData["weekly"].push(newTransaction);
-        }
-        if (period !== "monthly") {
-            backendData["monthly"].push(newTransaction);
-        }
+    setNewTransaction({
+      date: "",
+      category: "",
+      amount: 0,
+      type: "Income",
+    });
 
-        setNewTransaction({
-            date: "",
-            category: "",
-            amount: 0,
-            type: "Income",
-        });
+    localStorage.setItem("Budget", JSON.stringify(backendData));
+  };
 
-        localStorage.setItem("Budget", JSON.stringify(backendData));
-    };
-
-    return (
-        <div>
-            <h2>Add {newTransaction.type === "Income" ? "Income" : "Expense"}</h2>
-            <div>
-                <input
-                    type="date"
-                    value={newTransaction.date}
-                    onChange={(e) =>
-                        setNewTransaction({
-                            ...newTransaction,
-                            date: e.target.value,
-                        })
-                    }
-                />
-            </div>
-            <div>
-                <input
-                    type="text"
-                    placeholder="Category"
-                    value={newTransaction.category}
-                    onChange={(e) =>
-                        setNewTransaction({
-                            ...newTransaction,
-                            category: e.target.value,
-                        })
-                    }
-                />
-            </div>
-            <div>
-                <input
-                    placeholder="Amount"
-                    value={newTransaction.amount}
-                    onChange={(e) =>
-                        setNewTransaction({
-                            ...newTransaction,
-                            amount: parseFloat(e.target.value),
-                        })
-                    }
-                />
-            </div>
-            <div>
-                <label>
-                    <input
-                        type="radio"
-                        name="transactionType"
-                        value="Income"
-                        checked={newTransaction.type === "Income"}
-                        onChange={(e) =>
-                            setNewTransaction({
-                                ...newTransaction,
-                                type: e.target.value,
-                            })
-                        }
-                    />
-                    Income
-                </label>
-                <label>
-                    <input
-                        type="radio"
-                        name="transactionType"
-                        value="Expense"
-                        checked={newTransaction.type === "Expense"}
-                        onChange={(e) =>
-                            setNewTransaction({
-                                ...newTransaction,
-                                type: e.target.value,
-                            })
-                        }
-                    />
-                    Expense
-                </label>
-            </div>
-            <div>
-                <button onClick={handleAddTransaction} disabled={!isTransactionFormValid}>
-                    Add {newTransaction.type === "Income" ? "Income" : "Expense"}
-                </button>
-            </div>
-        </div>
-    );
+  return (
+    <div className="transaction-form">
+      <h2>Add {newTransaction.type === "Income" ? "Income" : "Expense"}</h2>
+      <div className="form-input">
+        <input
+          type="date"
+          value={newTransaction.date}
+          onChange={(e) =>
+            setNewTransaction({
+              ...newTransaction,
+              date: e.target.value,
+            })
+          }
+        />
+      </div>
+      <div className="form-input">
+        <input
+          type="text"
+          placeholder="Category"
+          value={newTransaction.category}
+          onChange={(e) =>
+            setNewTransaction({
+              ...newTransaction,
+              category: e.target.value,
+            })
+          }
+        />
+      </div>
+      <div className="form-input">
+        <input
+          placeholder="Amount"
+          value={newTransaction.amount}
+          onChange={(e) =>
+            setNewTransaction({
+              ...newTransaction,
+              amount: parseFloat(e.target.value),
+            })
+          }
+        />
+      </div>
+      <div className="form-input">
+        <label>
+          <input
+            type="radio"
+            name="transactionType"
+            value="Income"
+            checked={newTransaction.type === "Income"}
+            onChange={(e) =>
+              setNewTransaction({
+                ...newTransaction,
+                type: e.target.value,
+              })
+            }
+          />
+          Income
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="transactionType"
+            value="Expense"
+            checked={newTransaction.type === "Expense"}
+            onChange={(e) =>
+              setNewTransaction({
+                ...newTransaction,
+                type: e.target.value,
+              })
+            }
+          />
+          Expense
+        </label>
+      </div>
+      <div className="form-input">
+        <button
+          onClick={handleAddTransaction}
+          disabled={!isTransactionFormValid}
+        >
+          Add {newTransaction.type === "Income" ? "Income" : "Expense"}
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default RenderTransactionForm;
